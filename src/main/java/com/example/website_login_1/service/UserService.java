2a54f0019f7937cb170fe45f016dd085b7fcc5bc
neeld <neeldc2@gmail.com>
2025-01-26 15:55:00 +0530

    Initial commit

--- src/main/java/com/example/website_login_1/service/UserService.java
+++ src/main/java/com/example/website_login_1/service/UserService.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -50,7 +49,6 @@ public class UserService {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         //String jwtToken = jwtService.generateJwtToken(userDetails.getUsername());
         String jwtToken = jwtService.generateJwtToken(userLoginRequest.email());
-        System.out.println(jwtToken);
 
         return UserLoginResponse.builder()
                 .accessToken(jwtToken)
